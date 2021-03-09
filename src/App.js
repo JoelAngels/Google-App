ddcd1e62eead40eb1590b40ef84d3bbb5f6769a6
JoelAngels <joelbaraka4325@gmail.com>
2021-03-09 11:58:02 +0300

    Finished creaating and styling the header file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
-import Home from "./pages/Home";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      {/**Home (the one with search on => the entire page literally */}
-      <Home />
-      {/**Search Page (The results Page)*/}
+      <Router>
+        <Switch>
+          {/* {This will render the Search Component} */}
+          <Route path="/search">
+            <h1>This is a search page</h1>
+            {/* <SearchPage /> */}
+          </Route>
+          {/* {This will render the Home Component} */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        {/**Home (the one with search on => the entire page literally */}
+        {/**Search Page (The results Page)*/}
+      </Router>
     </div>
   );
 }
