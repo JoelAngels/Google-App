0c6b72b7021fdea7c1521b3bd4534460e9401f0c
JoelAngels <joelbaraka4325@gmail.com>
2021-03-09 10:48:30 +0300

    installed all needed dependencies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,19 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <div className="home__header"></div>
+      <div className="home__header">
+        <div className="home__left">
+          {/* 1 Link */}
+          {/* 1 Link */}
+        </div>
+        <div className="home__right">
+          {/* 1 Link */}
+          {/* 1 Link */}
+          {/* Icon */}
+          {/* Avatar */}
+        </div>
+      </div>
+      {/* //Everything underneath is the body */}
       <div className="home__body"></div>
     </div>
   );
