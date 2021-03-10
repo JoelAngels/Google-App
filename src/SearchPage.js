97a5404dcf1081771579f26e10a636ae85428a2a
JoelAngels <joelbaraka4325@gmail.com>
2021-03-10 15:10:12 +0300

    Added the Semi Links for the search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -5,6 +5,12 @@ import useGoogleSearch from "./useGoogleSearch";
 import response from "./response";
 import { Link } from "react-router-dom";
 import Search from "./Search";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function SearchPage() {
   //https://developers.google.com/custom-search/v1/using_rest
@@ -12,8 +18,8 @@ function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
 
   //LIVE API CALL
-  const { data } = useGoogleSearch(term);
-  //const data = response;
+  /*const { data } = useGoogleSearch(term);*/
+  const data = response;
 
   console.log(data);
   return (
@@ -31,8 +37,40 @@ function SearchPage() {
           <Search hideButtons textHidden />
 
           <div className="searchPage__options">
-            <div className="searchPage__right"></div>
-            <div className="searchPage__left"></div>
+            <div className="searchPage__optionsLeft">
+              <div className="searchPage__option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage__option">
+                <DescriptionIcon />
+                <Link to="/news">News</Link>
+              </div>
+              <div className="searchPage__option">
+                <ImageIcon />
+                <Link to="/images">Images</Link>
+              </div>
+              <div className="searchPage__option">
+                <LocalOfferIcon />
+                <Link to="/shopping">shopping</Link>
+              </div>
+              <div className="searchPage__option">
+                <RoomIcon />
+                <Link to="/maps">maps</Link>
+              </div>
+              <div className="searchPage__option">
+                <MoreVertIcon />
+                <Link to="/more">more</Link>
+              </div>
+            </div>
+            <div className="searchPage__optionsRight">
+              <div className="searchPage__option">
+                <Link to="/settings">Settings</Link>
+              </div>
+              <div className="searchPage__option">
+                <Link to="/tools">Tools</Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
